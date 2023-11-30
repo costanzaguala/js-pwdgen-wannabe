808d5e80d6259b3bb5743819019c118207cb3792
costanzaguala <costanzasilviaguala@gmail.com>
2023-11-30 16:19:27 +0100

    esercizio finito

--- Js/script.js
+++ Js/script.js
@@ -8,8 +8,6 @@ let LastName = prompt('Qual è il tuo cognome?');
 //FAVORITE COLOUR
 let FavoriteColour = prompt('Qual è il tuo colore preferito?');
 
-// NUMBER 
-
 const Password = FirstName + LastName + FavoriteColour + ('114');
 alert("La tua password è: " + FirstName + LastName + FavoriteColour + ('114'));
 document.writeln(Password)
